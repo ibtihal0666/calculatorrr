912aad892273341d5320c448ebf1ee076a593bc4
ibtihal <ibtihalnoui06@gmail.com>
2026-07-09 22:28:53 +0100

    Start calculator project

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int numbers[100];
+char op[4] = {'+','-','*','/'};
+int j;
+for(int i=0;i<100;i++)
+{
+    sum=numbers[i]+1;
+}
+
+int addition(){
+int s=0;
+for (i=0;i<100;i++)
+{
+   
+}
+return s;
+}

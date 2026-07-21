85f358a6512a28d948b1e10ab31037989d00aa90
ibtihal <ibtihalnoui06@gmail.com>
2026-07-21 23:34:11 +0100

    add and modifie

--- calculator.c
+++ calculator.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
-
+int main(){
 int numbers[100];
-char op[4] = {'+','-','*','/'};
-int j;
-for(int i=0;i<100;i++)
-{
-    sum=numbers[i]+1;
-}
+char operators[100];
+int n=0;
+int op=0;
+printf("Enter your number: ");
+scanf("%d",&numbers[n]);
+n=n+1;// table has a numbers when user enters a new number it will be add new space in the table
+while(1)
+  {
+  scanf("%c",&operators[op]);
+    if(operators[op]=='='){
+        break;
+    }
+    op=op+1;
+    scanf("%d",&numbers[n]);
+    n=n+1;
 
-int addition(){
-int s=0;
-for (i=0;i<100;i++)
-{
-   
-}
-return s;
+  }
 }

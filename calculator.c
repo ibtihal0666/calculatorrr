e1b327a34cc591b3679db8808d7be434125f709b
ibtihal <ibtihalnoui06@gmail.com>
2026-07-22 16:17:03 +0100

    add new

--- calculator.c
+++ calculator.c
@@ -7,15 +7,25 @@ int op=0;
 printf("Enter your number: ");
 scanf("%d",&numbers[n]);
 n=n+1;// table has a numbers when user enters a new number it will be add new space in the table
-while(1)
+while(1)// mean when the user input a number or operator is always true then go to the loop
   {
   scanf("%c",&operators[op]);
     if(operators[op]=='='){
-        break;
+        break;// when it's = stop and out and directly return
     }
-    op=op+1;
-    scanf("%d",&numbers[n]);
-    n=n+1;
+    op=op+1;// it's a table for operator when input a operator the table add new space in the table
+    scanf("%d",&numbers[n]);// add other num
+    n=n+1;// add other space in num's table
 
   }
+  return 0;// return the result
+   for (int i = 0; i < op; i++)
+   {
+     if (operators[i]=='*'||operators[i]=='/')
+     {
+      numbers[i]=numbers[i]*numbers[i+1];
+     }
+     
+   }
+   
 }

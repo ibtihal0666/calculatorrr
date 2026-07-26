d02752571baa9dcffd70e409d4304cf17e869e14
ibtihal <ibtihalnoui06@gmail.com>
2026-07-26 21:06:29 +0100

    add devision and change

--- calculator.c
+++ calculator.c
@@ -37,7 +37,15 @@ while(1)// mean when the user input a number or operator is always true then go 
    {
      if (operators[i]=='*'||operators[i]=='/')
      {
+      if (operators[i]=='*')
+      {
       multiplication(numbers[i],numbers[i+1]);
+      }
+      if (operators[i]=='/')
+      {
+        devision(numbers[i],numbers[i+1]);
+      }
+      
        for (int j= i+1; j<n; j++)
        {
          
